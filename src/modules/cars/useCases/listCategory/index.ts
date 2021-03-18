2d7548c4f053653f80824a2dbfc3538aae5615ed
Eric Miranda <emribeiro@github.com>
2021-03-18 11:20:41 -0300

    Change CategoriesRepository to Singleton

--- src/modules/cars/useCases/listCategory/index.ts
+++ src/modules/cars/useCases/listCategory/index.ts
@@ -3,7 +3,7 @@ import { ListCategoryController } from "./ListCategoryController";
 import { ListCategoryUseCase } from "./ListCategoryUseCase";
 
 
-const categoryRepository = new CategoriesRepository();
+const categoryRepository = CategoriesRepository.getInstance();
 const listCategoryUseCase = new ListCategoryUseCase(categoryRepository);
 const listCategoryController = new ListCategoryController(listCategoryUseCase);
 
