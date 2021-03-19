dbfe58589680b073ad916b42bc72337474377a87
Eric Miranda <emribeiro@github.com>
2021-03-19 10:01:14 -0300

    include import categories files operation

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,8 +1,13 @@
 import { Router } from 'express';
 import { createCategoryController } from '../modules/cars/useCases/createCategory/index'
 import { listCategoryController } from '../modules/cars/useCases/listCategory';
+import multer from 'multer';
 
 const categoriesRouter = Router()
+
+const upload = multer({
+    dest: './tmp',
+});
 
 categoriesRouter.post("/", (request, response) => {
     return createCategoryController.handle(request, response);
@@ -10,6 +15,14 @@ categoriesRouter.post("/", (request, response) => {
 
 categoriesRouter.get("/", (request, response) => {
     return listCategoryController.handle(request, response);
+});
+
+categoriesRouter.post("/import", upload.single("file"), (request, response) => {
+    const { file } = request;
+    console.log(file);
+    
+    
+    return response.status(200).send();
 });
 
 export {categoriesRouter};
