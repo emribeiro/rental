53aa0088691323e259a1662c148dc61ed3d98a56
Eric Miranda <emribeiro@github.com>
2021-03-29 11:28:33 -0300

    User migration / User Entity Creation

---
+++ src/database/migrations/1617027028520-CreateUsers.ts
@@ -0,0 +1,52 @@
+import {MigrationInterface, QueryRunner, Table} from "typeorm";
+
+export class CreateUsers1617027028520 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.createTable( new Table({
+            name: "users",
+            columns: [
+                {
+                    name: "id",
+                    type: "uuid",
+                    isPrimary: true
+                },
+                {
+                    name: "name",
+                    type: "varchar"
+                },
+                {
+                    name: "username",
+                    type: "varchar",
+                    isUnique: true
+                },
+                {
+                    name: "password",
+                    type: "varchar"
+                },
+                {
+                    name: "email",
+                    type: "varchar"
+                },
+                {
+                    name: "driver_license",
+                    type: "varchar"
+                },
+                {
+                    name: "admin",
+                    type: "boolean"
+                },
+                {
+                    name: "created_at",
+                    type: "date",
+                    default: "now()"
+                }
+            ]
+        }));
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.dropTable("users");
+    }
+
+}
