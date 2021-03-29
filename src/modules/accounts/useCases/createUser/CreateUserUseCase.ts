4c675da734af8f3954476b07fde345464c7bb6b4
Eric Miranda <emribeiro@github.com>
2021-03-29 16:21:52 -0300

    User Password Cryptograph

--- src/modules/accounts/useCases/createUser/CreateUserUseCase.ts
+++ src/modules/accounts/useCases/createUser/CreateUserUseCase.ts
@@ -1,3 +1,4 @@
+import { hash } from "bcryptjs"
 import { inject, injectable } from "tsyringe";
 import { ICreateUserDTO } from "../../dtos/ICreateUserDTO";
 import { User } from "../../model/User";
@@ -12,9 +13,12 @@ class CreateUserUseCase{
     ){};
 
     async execute({name, password, email, driver_license}: ICreateUserDTO): Promise<User>{
+
+        const passwordHash = await hash(password, 8);
+
         const user = await this.userRepository
                                 .create({ name,
-                                          password, 
+                                          password: passwordHash, 
                                           email, 
                                           driver_license}); 
         
