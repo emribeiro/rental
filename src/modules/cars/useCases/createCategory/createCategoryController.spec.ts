8bcc397ca299d6a7e39867fd9c02a76f68e94948
Eric Miranda <emribeiro@github.com>
2021-04-19 10:25:58 -0300

    Create Category Tests

---
+++ src/modules/cars/useCases/createCategory/createCategoryController.spec.ts
@@ -0,0 +1,51 @@
+import request from "supertest";
+import { Connection, createConnection } from "typeorm";
+import { app } from "../../../../app";
+import { v4 as uuidV4} from 'uuid';
+import { hash } from "bcryptjs";
+
+
+let connection : Connection;
+
+describe("Create category Controle", () => {
+
+    beforeAll(async () => {
+        connection = await createConnection();
+        await connection.runMigrations();
+
+        const id = uuidV4();
+        const password = await hash("admin", 8);
+
+
+        await connection.query(
+            `INSERT INTO USERS(id,name, email, password, admin, created_at, driver_license)
+            values ('${id}', 'admin', 'admin@admin.com', '${password}', true, 'now()', 'XXXXXXXX');
+            `
+        )
+            });
+
+    afterAll( async () => {
+        await connection.dropDatabase();
+        await connection.close();
+    });
+
+    it("Should be able to create a new category", async () => {
+
+        const responseToken = await request(app).post("/sessions").send({
+            email: "admin@admin.com",
+            password: "admin"
+        });
+
+        const { token } = responseToken.body;
+
+        const response = await request(app).post("/categories").send({
+            name: "Category SuperTest",
+            description: "Category SuperTest Description."
+        }).set({
+            Authorization: `Bearer ${token}`
+        });
+
+        expect(response.status).toBe(201);
+
+    })
+});
