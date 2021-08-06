ea217cf070750da65253933b04debf304da976ff
Eric Miranda Ribeiro <ktoxlonian@gmail.com>
2021-08-06 11:40:31 -0300

    sentry configuration

--- src/app.ts
+++ src/app.ts
@@ -13,16 +13,36 @@ import instance from "tsyringe/dist/typings/dependency-container";
 import { AppError } from "./errors/AppError";
 import upload from "./config/upload";
 import cors from "cors";
+import rateLimiter from "./shared/infra/http/middlewares/rateLimiter";
+
+import * as Sentry from "@sentry/node";
+import * as Tracing from "@sentry/tracing";
 
 createConnection();
 
 const app = express();
 
+Sentry.init({
+    dsn: process.env.SENTRY_DSN,
+    integrations: [
+      new Sentry.Integrations.Http({ tracing: true }),
+      new Tracing.Integrations.Express({ app }),
+    ],
+    tracesSampleRate: 1.0,
+  });
+
 app.use(express.json());
+app.use(rateLimiter)
+
+app.use(Sentry.Handlers.requestHandler());
+app.use(Sentry.Handlers.tracingHandler());
+
 app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerFile));
 app.use("/avatar", express.static(`${upload.tmpFolder}/avatar`));
 app.use("/avatar", express.static(`${upload.tmpFolder}/cars`));
 app.use(cors());
+
+app.use(Sentry.Handlers.errorHandler());
 
 app.use(router);
 
