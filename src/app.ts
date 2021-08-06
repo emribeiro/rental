086183b5cd325fc121674aea72dcb1fe642152aa
Eric Miranda Ribeiro <ktoxlonian@gmail.com>
2021-08-06 11:00:00 -0300

    cors inclusion

--- src/app.ts
+++ src/app.ts
@@ -12,6 +12,7 @@ import "./shared/container"
 import instance from "tsyringe/dist/typings/dependency-container";
 import { AppError } from "./errors/AppError";
 import upload from "./config/upload";
+import cors from "cors";
 
 createConnection();
 
@@ -21,6 +22,8 @@ app.use(express.json());
 app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerFile));
 app.use("/avatar", express.static(`${upload.tmpFolder}/avatar`));
 app.use("/avatar", express.static(`${upload.tmpFolder}/cars`));
+app.use(cors());
+
 app.use(router);
 
 app.use((err: Error, request: Request, response: Response, next: NextFunction) => {
